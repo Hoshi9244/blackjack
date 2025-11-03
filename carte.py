77eba7f81802c48dc846815b6cc0decfe31a1273
Hoshi <carlvernierespro@gmail.com>
2025-11-03 10:48:27 +0100

    Delete carte.py

--- carte.py
+++
@@ -1,8 +0,0 @@
-class Carte:
-    
-    def __init__(self, nom, valeur):
-        self.nom = nom       # le nom de cette carte uniquement, ex: "K"
-        self.valeur = valeur # la valeur numérique de la carte, ex: 10
-
-    def __repr__(self):
-        return f"Carte('{self.nom}', {self.valeur})"
